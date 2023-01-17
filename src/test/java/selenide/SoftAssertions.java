851736624d85de209fb9fa2c6cef0f1b4f7bbfca
davikr7 <david.krasnov7@gmail.com>
2023-01-18 00:27:54 +0400

    check JUnit5 appearance

--- src/test/java/selenide/SoftAssertions.java
+++ src/test/java/selenide/SoftAssertions.java
@@ -1,7 +1,27 @@
 package selenide;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
 public class SoftAssertions {
-    
+    @Test
+    void checkAssertion() {
+
+        // Откройте страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+        // Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+        // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(".Box--condensed").shouldHave(text("SoftAssertions"));
+        // Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("JUnit5"));
+    }
 }
